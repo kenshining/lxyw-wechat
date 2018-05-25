333fd3ed7d9bf7b5ee086c5a10f9ab12b2f4f6c3
KenCheng <kencheng@foxmail.com>
2018-05-25 16:09:05 +0800

    初始化导入

---
+++ pages/news/new_products.js
@@ -0,0 +1,88 @@
+// pages/news/new_products.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+    pomotes : [
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
+      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+    ],
+    news : [
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg', id:'热烈庆祝我司5月上海国际食品展获圆满成功',title:'',subTitle:''},
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg', id: '热烈庆祝我司5月上海国际食品展获圆满成功', title: '', subTitle: '' },
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg', id: '热烈庆祝我司5月上海国际食品展获圆满成功', title: '', subTitle: '' },
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg', id: '热烈庆祝我司5月上海国际食品展获圆满成功', title: '', subTitle: '' },
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg', id: '热烈庆祝我司5月上海国际食品展获圆满成功', title: '', subTitle: '' },
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg', id: '热烈庆祝我司5月上海国际食品展获圆满成功', title: '', subTitle: '' }
+    ]
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+  /**
+   * 加载更多新闻
+   */
+  loadMore : function(){
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
